43ab9d04ea78ea853ff1c74f8276ca7d8d05ac86
Eliel Fernandes <eliel.fma@gmail.com>
2015-05-12 13:56:13 -0300

    wa com upload

--- application/modules/modelos/controllers/WalkaroundController.php
+++ application/modules/modelos/controllers/WalkaroundController.php
@@ -1,19 +1,30 @@
 <?php
+use google\appengine\api\cloud_storage\CloudStorageTools;
 class Modelos_WalkaroundController extends Zend_Controller_Action{
     public function init(){
     	$this->_helper->layout()->setLayout('modelos');
     }
     public function indexAction(){}
     public function geralAction(){
-        //$options = ['gs_bucket_name' => 'mb-cadastro-arquivos'];
-		//$this->view->upload_url = CloudStorageTools::createUploadUrl('/modelos/walkaround/uploadpopup', $options);
-        $obj = new SC_UploadGae();
+        /*$options = ['gs_bucket_name' => 'mb-wa-images'];
+		$this->view->upload_url = CloudStorageTools::createUploadUrl('/modelos/walkaround/uploadpopup', $options);*/
     }
     public function fotosAction(){}
     public function uploadpopupAction(){
     	Zend_Layout::resetMvcInstance();
         $this->_helper->viewRenderer->setNoRender(true);
+        $modeloid = new Zend_Session_Namespace('modeloselecionado');
+
+        $bucket    = 'mb-wa-images';
+        $root_path = 'gs://' . $bucket . '/pop_ups';
+        $name = $_FILES['arquivo']['name'];
+        $original = $root_path . '/' . $name;
+        $tmp = $_FILES['arquivo']['tmp_name'];
         
-    	print_r($_FILES['arquivo']['name']);
+        if(move_uploaded_file($tmp, $original)){
+            echo 'funfo ' . $modeloid->id;
+        }else{
+            echo 'nao rolou';
+        }
     }
 }
--- library/SC/Plugins/UploadGae.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('../google/appengine/api/cloud_storage/CloudStorageTools.php');
-namespace google\appengine\api\cloud_storage\CloudStorageTools;
-
-class SC_UploadGae extends Zend_Controller_Plugin_Abstract{
-
-}
