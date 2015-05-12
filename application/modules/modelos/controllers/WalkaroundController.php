869900fde5858e240ebdf17ad6b6ff28da694cc1
Eliel Fernandes <eliel.fma@gmail.com>
2015-05-12 09:13:30 -0300

    WA update

--- application/modules/modelos/controllers/WalkaroundController.php
+++ application/modules/modelos/controllers/WalkaroundController.php
@@ -4,6 +4,16 @@ class Modelos_WalkaroundController extends Zend_Controller_Action{
     	$this->_helper->layout()->setLayout('modelos');
     }
     public function indexAction(){}
-    public function geralAction(){}
-    public function fotosAction(){}    
+    public function geralAction(){
+        //$options = ['gs_bucket_name' => 'mb-cadastro-arquivos'];
+		//$this->view->upload_url = CloudStorageTools::createUploadUrl('/modelos/walkaround/uploadpopup', $options);
+        $obj = new SC_UploadGae();
+    }
+    public function fotosAction(){}
+    public function uploadpopupAction(){
+    	Zend_Layout::resetMvcInstance();
+        $this->_helper->viewRenderer->setNoRender(true);
+        
+    	print_r($_FILES['arquivo']['name']);
+    }
 }
