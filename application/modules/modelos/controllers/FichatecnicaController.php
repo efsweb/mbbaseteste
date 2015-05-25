c2de9ec285d5cad1059fdc94bbd72a2ba567bc6e
Eliel Fernandes <eliel.fma@gmail.com>
2015-05-25 11:19:37 -0300

    conteudo atualizado

--- application/modules/modelos/controllers/FichatecnicaController.php
+++ application/modules/modelos/controllers/FichatecnicaController.php
@@ -19,6 +19,17 @@ class Modelos_FichatecnicaController extends Zend_Controller_Action{
         }
         $modelo = new Model_Geralft();
         $this->view->content = $modelo->loaddata($mdid,$pgid);
+
+        $modelos = new Model_ModelosMapper();
+        $familias = $modelos->lista();
+        foreach ($familias as $key => $value) {
+            $arr = $modelos->lista2('modelo',$value);
+            if(array_key_exists($modelo->id, $arr)){
+                echo $modelo->id;
+            }
+        }
+        exit();
+
     }
     public function caixadetransferenciaAction(){
         $pgid = 'mod_ft_caixa_transferencia';
@@ -160,24 +171,23 @@ class Modelos_FichatecnicaController extends Zend_Controller_Action{
      */
     public function searchAction(){
         Zend_Layout::resetMvcInstance();
-        $this->_helper->viewRenderer->setNoRender(true);
         
-        $param_desc_alias = $this->_getParam('param_desc_alias', '');
-        $param_finalidade = $this->_getParam('param_finalidade', 'label');
-        $param_tipo       = $this->_getParam('param_tipo', 'alias');
-        $param_grupo      = $this->_getParam('param_param_grupo', 'Todos');
-        $param_classif    = $this->_getParam('param_clasi', 'Todos');
+        $this->_helper->viewRenderer->setNoRender(true);
+        $nome = $this->getRequest()->getParam('nomefile', '');
 
-        $modelos = new Model_ModelosMapper();
-        $result  = $modelos->searchalias($param_desc_alias, $param_finalidade, $param_tipo, $param_grupo, $param_classif);
-        //$result  = $modelos->searchalias();
-        /*echo '<pre>';
-        print_r($result);
-        echo '</pre>';*/
-        /*$itens   = array();
-        foreach($result as $item){
-            $itens[]   = get_object_vars($item);
-        }*/
-        echo json_encode($result);
+        $bucket    = 'mb-ft-images';
+        $root_path = 'gs://' . $bucket . '/';
+        $original = $root_path . $nome;
+        $tmp = $_FILES['arquivo']['tmp_name'];
+
+        CloudStorageTools::deleteImageServingUrl($original);
+        unlink($original);
+
+        if(move_uploaded_file($tmp, $original)){
+            $arr  = array('status' => 'ok');
+            echo json_encode($arr);
+       }else{
+           echo 'Erro ao fazer o upload da imagem';
+       }
     }
 }
